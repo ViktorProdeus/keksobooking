9b2e5b04c7683f9ee7290c468b3226544b1d05b8
ViktorProdeus <viktorprodeus@gmail.com>
2024-12-27 01:19:42 +0300

    feat: Модуляция

---
+++ js/data.js
@@ -0,0 +1,60 @@
+import {
+  getAuthor,
+  getLocation,
+  getRandomFloat,
+  getRandomInt,
+  shuffle,
+} from './utils.js';
+
+const COUNT = 10;
+const APARTMENT_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const DESCRIPTION = [
+  'Милая, уютная квартирка в центре Токио',
+  'Уютное гнездышко для молодоженов',
+  'Красивый, уютный дворец в центре',
+  'Бунгало в саду',
+  'Маленький отель',
+  'Пентхаус с высотой крыши',
+];
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+function createRentalAdd() {
+  const location = getLocation(
+    getRandomFloat(35.65, 35.7, 5),
+    getRandomFloat(139.7, 139.8, 5)
+  );
+
+  return {
+    author: getAuthor(getRandomInt(1, COUNT)),
+    offer: {
+      title: 'Аренда жилья',
+      address: `${location.lat}, ${location.lng}`,
+      description: DESCRIPTION[getRandomInt(0, DESCRIPTION.length - 1)],
+      price: getRandomInt(10000, 80000),
+      type: APARTMENT_TYPES[getRandomInt(0, APARTMENT_TYPES.length - 1)],
+      rooms: getRandomInt(1, 5),
+      guests: getRandomInt(1, 8),
+      checkin: `${getRandomInt(12, 14)}:00`,
+      checkout: `${getRandomInt(12, 14)}:00`,
+      features: shuffle(FEATURES).slice(0, getRandomInt(1, FEATURES.length)),
+      photos: shuffle(PHOTOS).slice(0, getRandomInt(1, PHOTOS.length)),
+    },
+    location,
+  };
+}
+
+const rentalAdds = Array.from({ length: COUNT }, createRentalAdd);
+
+export { rentalAdds };
