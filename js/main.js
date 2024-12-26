99d57baf29a15abaa113ef837059478f1446b3a0
ViktorProdeus <viktorprodeus@gmail.com>
2024-12-27 00:59:12 +0300

    feat: Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,22 +1,135 @@
-function calculateCondition(min, max) {
-  if (min < 0 || max < 0) {
-    throw new Error('min и max должны быть неотрицательными');
+// Функция взята из интернета и доработана
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+
+function getRandomPositiveInteger(a, b) {
+  // Чтобы не заставлять пользователя нашей функции помнить порядок аргументов,
+  // реализуем поддержку передачи минимального и максимального значения в любом порядке,
+  // а какое из них большее и меньшее вычислим с помощью Math.min и Math.max.
+
+  // После нам нужно убедиться, что пользователь не передал дробные значения,
+  // для этого на всякий пожарный случай нижнюю границу диапазона
+  // мы округляем к ближайшему большему целому с помощью Math.ceil,
+  // а верхнюю границу - к ближайшему меньшему целому с помощью Math.floor
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  // Обратите внимание, чтобы учесть условие, что диапазон может быть [0, ∞),
+  // мы не ругаем пользователя за переданное отрицательное число,
+  // а просто берём его по модулю с помощью Math.abs
+
+  // Дальше используем Math.random() для получения случайного дробного числа в диапазоне [0, 1),
+  // которое домножаем на разницу между переданными числами плюс единица - это будет наша случайная дельта.
+  // После нужно сложить дельту с минимальным значением, чтобы получить итоговое случайное число.
+  const result = Math.random() * (upper - lower + 1) + lower;
+  // "Плюс единица", чтобы включить верхнюю границу диапазона в случайные числа
+
+  // И в конце с помощью метода Math.floor мы округляем полученный результат,
+  // потому что Math.random() генерирует только дробные числа и ноль.
+  return Math.floor(result);
+}
+
+// Функция взята из интернета и доработана
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+
+function getRandomPositiveFloat(a, b, digits = 1) {
+  // Чтобы не заставлять пользователя нашей функции помнить порядок аргументов,
+  // реализуем поддержку передачи минимального и максимального значения в любом порядке,
+  // а какое из них большее и меньшее вычислим с помощью Math.min и Math.max
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  // Обратите внимание, чтобы учесть условие, что диапазон может быть [0, ∞),
+  // мы не ругаем пользователя за переданное отрицательное число,
+  // а просто берём его по модулю с помощью Math.abs
+
+  // Дальше используем Math.random() для получения случайного дробного числа в диапазоне [0, 1),
+  // которое домножаем на разницу между переданными числами - это будет наша случайная дельта.
+  // После нужно сложить дельту с минимальным значением, чтобы получить итоговое случайное число.
+  const result = Math.random() * (upper - lower) + lower;
+
+  // И в конце с помощью метода toFixed любого числа в JavaScript
+  // указать требуемое количество знаков после точки.
+  // Метод возвращает строку, поэтому с помощью унарного плюса превращаем её в число
+  return +result.toFixed(digits);
+}
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
-  if (min > max) {
-    [min, max] = [max, min];
+  return array;
+}
+
+function getAuthor(count) {
+  if (count < 10) {
+    count = `0${count}`;
   }
+  return {
+    avatar: `img/avatars/user${count}.png`,
+  };
 }
 
-function getRandomInt(min, max) {
-  calculateCondition(min, max);
-  return min + Math.floor(Math.random() * (max - min + 1));
+const COUNT = 10;
+const APARTMENT_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const DESCRIPTION = [
+  'Милая, уютная квартирка в центре Токио',
+  'Уютное гнездышко для молодоженов',
+  'Красивый, уютный дворец в центре',
+  'Бунгало в саду',
+  'Маленький отель',
+  'Пентхаус с высотой крыши',
+];
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+function getLocation(lat, lng) {
+  return {
+    lat,
+    lng,
+  };
 }
 
-getRandomInt(1, 10);
+function createRentalAdd() {
+  const location = getLocation(
+    getRandomPositiveFloat(35.65, 35.7, 5),
+    getRandomPositiveFloat(139.7, 139.8, 5)
+  );
 
-function getRandomFloat(min, max, decimals = 1) {
-  calculateCondition(min, max);
-  return +(Math.random() * (max - min) + min).toFixed(decimals);
+  return {
+    author: getAuthor(getRandomPositiveInteger(1, COUNT)),
+    offer: {
+      title: 'Аренда жилья',
+      address: `${location.lat}, ${location.lng}`,
+      description:
+        DESCRIPTION[getRandomPositiveInteger(0, DESCRIPTION.length - 1)],
+      price: getRandomPositiveInteger(10000, 80000),
+      type: APARTMENT_TYPES[
+        getRandomPositiveInteger(0, APARTMENT_TYPES.length - 1)
+      ],
+      rooms: getRandomPositiveInteger(1, 5),
+      guests: getRandomPositiveInteger(1, 8),
+      checkin: `${getRandomPositiveInteger(12, 14)}:00`,
+      checkout: `${getRandomPositiveInteger(12, 14)}:00`,
+      features: shuffle(FEATURES).slice(
+        0,
+        getRandomPositiveInteger(1, FEATURES.length)
+      ),
+      photos: shuffle(PHOTOS).slice(
+        0,
+        getRandomPositiveInteger(1, PHOTOS.length)
+      ),
+    },
+    location,
+  };
 }
 
-getRandomFloat(1, 10, 2);
+Array.from({ length: COUNT }, createRentalAdd);
